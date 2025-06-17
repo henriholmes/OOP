24e0141211b365075e0f84b9903cd39dd6991f67
Dominik <dominikkrueger06@gmail.com>
2025-06-17 11:38:17 +0200

    gg

--- util/Database.java
+++ util/Database.java
@@ -7,16 +7,14 @@ import java.sql.SQLException;
 
 
     public class Database {
-    private static final String URL = "jdbc:sqlite:studenten.db";
+  private static final String URL = "jdbc:sqlite:studenten.db";
 
     public static Connection connect() throws SQLException {
         try {
-            // ⬇️ SQLite JDBC-Treiber explizit laden
             Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException e) {
             throw new SQLException("SQLite-Treiber nicht gefunden", e);
         }
-
         return DriverManager.getConnection(URL);
     }
 }
